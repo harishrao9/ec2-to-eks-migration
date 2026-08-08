eae3ca6db2ac0bcae420aab8e36fb772252e622f
harishrao9 <raoharish116@gmail.com>
2026-08-08 11:50:47 +0530

    feat: add Kubernetes manifests and Helm chart

--- terraform/eks/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "foodrush-tfstate-harishrao9"
-    key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "foodrush-tfstate-lock"
-    encrypt        = true
-  }
-}
